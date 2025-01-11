6db0e3e6408575e48733a90f77f71dc667ea5ea8
Elysium28 <matthew@leo.bzh>
2025-01-11 18:24:01 +0100

    update homepage design

--- site web/script.js
+++ site web/script.js
@@ -1,12 +1,24 @@
- // Sélection du header
- const header = document.querySelector('.header');
+const header = document.querySelector('.header');
+const headerSection = document.querySelector('.header > section');
 
- // Ajout d'un événement au défilement
- window.addEventListener('scroll', () => {
-     if (window.scrollY > 50) { // Vérifie si on a défilé de 100px
-         header.classList.add('shrink');
-     } else {
-         header.classList.remove('shrink');
-     }
- });
- 
+// Ajout d'un événement au défilement pour le header
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Vérifie si on a défilé de 50px
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
+
+// Ajout d'un événement au défilement pour appliquer l'effet de flou au header
+window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const blurValue = Math.min(scrollPosition / 100, 10); // Ajuste le diviseur et la valeur max si nécessaire
+    header.style.setProperty('--blur-value', `${blurValue}px`);
+    headerSection.style.setProperty('--blur-value', `${blurValue}px`);
+});
+
+
+
+
+
